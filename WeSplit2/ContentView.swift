c30528942f0bcabbb11d3a8ac821264b821da857
David Herrero <daheji@gmail.com>
2019-10-30 14:30:32 +0100

    Working with forms

--- WeSplit2/ContentView.swift
+++ WeSplit2/ContentView.swift
@@ -5,13 +5,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var tapCount = 0
+    
     var body: some View {
-        NavigationView{
-            Form{
-                Section{
-                    Text("PepeWorld")
-                }
-            }.navigationBarTitle("SwiftUI", displayMode: .inline)
+        
+        VStack{
+            
+            NavigationView{
+
+                        Text("lalal").navigationBarTitle("SwiftUI", displayMode: .inline)
+            }
+            
+            
+            
+            Button("Tap Count: \(tapCount)"){
+                self.tapCount += 1
+            }
+            
+//            Form{
+//                
+//                Text("My first app").font(.title)
+//                Text("Tap the button below").font(.subheadline)
+//                TextField("Enter your name")
+//                Text("lslalas")
+//            }
+            
+
         }
     }
 }
