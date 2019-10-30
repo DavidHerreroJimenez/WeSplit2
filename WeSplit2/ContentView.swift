cd4b351fcc7bf862d2cfdc1a9b8297785e91d60a
David Herrero <daheji@gmail.com>
2019-10-30 09:59:11 +0100

    Initial Commit

---
+++ WeSplit2/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WeSplit2
+//
+//  Created by Mati on 30/10/2019.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
