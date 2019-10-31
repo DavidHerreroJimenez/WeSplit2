ebf083f63bac8722c1786781233b1f6fa02d264e
David Herrero <daheji@gmail.com>
2019-10-31 10:12:52 +0100

    Program state modified

--- WeSplit2/ContentView.swift
+++ WeSplit2/ContentView.swift
@@ -7,31 +7,19 @@ import SwiftUI
 struct ContentView: View {
     
     @State var tapCount = 0
+    @State private var myName = ""
     
     var body: some View {
         
         VStack{
             
             NavigationView{
-
-                        Text("lalal").navigationBarTitle("SwiftUI", displayMode: .inline)
+                
+                Button("Tap Count: \(tapCount)"){
+                    self.tapCount += 1
+                }
+      
             }
-            
-            
-            
-            Button("Tap Count: \(tapCount)"){
-                self.tapCount += 1
-            }
-            
-//            Form{
-//                
-//                Text("My first app").font(.title)
-//                Text("Tap the button below").font(.subheadline)
-//                TextField("Enter your name")
-//                Text("lslalas")
-//            }
-            
-
         }
     }
 }
