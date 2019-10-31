fba310df449c4f7dfae442d8fb2eb926cd44fd81
David Herrero <daheji@gmail.com>
2019-10-31 10:57:03 +0100

    Segmented control was added

--- WeSplit2/ContentView.swift
+++ WeSplit2/ContentView.swift
@@ -6,7 +6,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-
+    
     
     @State private var checkAmount = ""
     @State private var numberOfPeople = 2
@@ -17,25 +17,36 @@ struct ContentView: View {
     var body: some View {
         
         NavigationView{
-        
-        Form{
-            Section{
-                TextField("Amount", text: $checkAmount).keyboardType(.decimalPad)
-                
-                Picker("Number of people", selection: $numberOfPeople){
-                    ForEach(2 ..< 100){
-                        Text("\($0) people")
+            
+            Form{
+                Section{
+                    TextField("Amount", text: $checkAmount).keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2 ..< 100){
+                            Text("\($0) people")
+                        }
                     }
+                    
+                    
                 }
-            }
-            Section{
-                Text("$\(checkAmount)")
-            }
+                Section{
+                    Text("How much tip do you want to leave?")
+                    
+                    Picker("Tip pecentage", selection: $tipPercentage){
+                        ForEach(0 ..< tipPercentages.count){
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                Section{
+                    Text("$\(checkAmount)")
+                }
             }.navigationBarTitle("WeSplit")
             
         }
         
-     
+        
     }
 }
 
